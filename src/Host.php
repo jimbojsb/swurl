fe07f6ddabbf6ffcd5052d82512be4024ae6859e
Josh Butts <josh@joshbutts.com>
2014-12-23 09:26:00 -0600

    Add port number and IP support for host. Switch to PSR-4

---
+++ src/Host.php
@@ -0,0 +1,102 @@
+<?php
+namespace Swurl;
+
+class Host
+{
+    private $parts = [];
+    private $isIpAddress = false;
+    private $port;
+
+    public function __construct($hostname = null)
+    {
+        if ($hostname) {
+            if (filter_var($hostname, FILTER_VALIDATE_IP)) {
+                $this->isIpAddress = true;
+            }
+
+            if (strpos($hostname, ":") !== false) {
+                list($hostname, $port) = explode(":", $hostname);
+                $this->port = $port;
+            }
+
+            $hostnameParts = explode('.', $hostname);
+            $this->parts = $hostnameParts;
+        }
+    }
+
+    public function isIpAddress()
+    {
+        return $this->isIpAddress;
+    }
+
+    public function hasPort()
+    {
+        return is_numeric($this->port);
+    }
+
+    public function getPort()
+    {
+        return $this->port;
+    }
+
+    public function setPort($port = null)
+    {
+        if (is_numeric($port) || is_null($port)) {
+            $this->port = $port;
+        } else {
+            throw new \InvalidArgumentException("Cannot set non-numeric port");
+        }
+    }
+
+    public function getTld()
+    {
+        if ($this->isIpAddress) {
+            return $this->__toString();
+        }
+        return $this->parts[count($this->parts) - 1];
+    }
+
+    public function __toString()
+    {
+        $hostname =  implode(".", $this->parts);
+        if ($this->hasPort()) {
+            $hostname .= ":$this->port";
+        }
+        return $hostname;
+    }
+
+    public function addSubdomain($domain)
+    {
+        if ($this->isIpAddress) {
+            throw new \RuntimeException("cannot do subdomain manipulation on an IP-based host");
+        }
+        if (count($this->parts) == 127) {
+            throw new \RuntimeException("domain names may have at a maximum 127 components");
+        }
+        if (strlen($this->__toString()) . ".$domain" > 253) {
+            throw new \RuntimeException("max length of domain names is 253 characters");
+        }
+
+        array_unshift($this->parts, $domain);
+    }
+
+    public function removeSubdomain($domain)
+    {
+        if ($this->isIpAddress) {
+            throw new \RuntimeException("cannot do subdomain manipulation on an IP-based host");
+        }
+        $subdomainIndex = array_search($domain, $this->parts);
+        if ($subdomainIndex !== false) {
+            unset($this->parts[$subdomainIndex]);
+        }
+        $this->parts = array_values($this->parts);
+    }
+
+    public function hasSubdomain($domain)
+    {
+        if ($this->isIpAddress) {
+            return false;
+        }
+        return array_search($domain, $this->parts) !== false;
+    }
+}
--- tests/HostTest.php
+++ tests/HostTest.php
@@ -29,6 +29,13 @@ class HostTest extends PHPUnit_Framework_TestCase
         $h->addSubdomain('www');
         $this->assertTrue($h->hasSubdomain('www'));
         $this->assertEquals('www.example.com', $h->__toString());
+
+        $h = new Host("10.0.0.1");
+        try {
+            $h->removeSubdomain("foo");
+            $this->fail("Should have thrown runtime exception trying to modify IP hostname");
+        } catch (\RuntimeException $e) {
+        }
     }
 
     public function testRemoveSubdomain()
@@ -37,5 +44,42 @@ class HostTest extends PHPUnit_Framework_TestCase
         $h->removeSubdomain('www');
         $this->assertFalse($h->hasSubdomain('www'));
         $this->assertEquals('example.com', $h->__toString());
+
+        $h = new Host("10.0.0.1");
+        try {
+            $h->removeSubdomain("foo");
+            $this->fail("Should have thrown runtime exception trying to modify IP hostname");
+        } catch (\RuntimeException $e) {
+        }
+    }
+
+    public function testIsIpAddress()
+    {
+        $h = new Host("www.example.com");
+        $this->assertFalse($h->isIpAddress());
+
+        $h = new Host("192.168.1.1");
+        $this->assertTrue($h->isIpAddress());
+    }
+
+    public function testHostWithPort()
+    {
+        $h = new Host("www.example.com:8080");
+        $this->assertTrue($h->hasPort());
+        $this->assertEquals("8080", $h->getPort());
+
+        $h->setPort(9090);
+        $this->assertEquals(9090, $h->getPort());
+
+        $h->setPort();
+        $this->assertFalse($h->hasPort());
+        $this->assertEquals(null, $h->getPort());
+
+        try {
+            $h->setPort("Foo");
+            $this->fail("Expected InvalidArgumentException trying to set non-numeric port");
+        } catch (\InvalidArgumentException $e) {
+        }
+
     }
 }
