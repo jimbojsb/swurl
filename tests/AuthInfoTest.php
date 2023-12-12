27ded177f6b3c453e46ff14d1c7282caca0e88ca
Jay Fuqua <archangel2080@gmail.com>
2023-12-12 13:44:57 -0600

    handle '0' value

--- tests/AuthInfoTest.php
+++ tests/AuthInfoTest.php
@@ -25,6 +25,12 @@ class AuthInfoTest extends TestCase
         $a = new AuthInfo('foo');
         $this->assertEquals('foo', $a->__toString());
 
+        $a = new AuthInfo('foo:0');
+        $this->assertEquals('foo:0', $a->__toString());
+
+        $a = new AuthInfo('0:bar');
+        $this->assertEquals('0:bar', $a->__toString());
+
         $a = new AuthInfo(['foo', 'bar']);
         $this->assertEquals('foo:bar', $a->__toString());
 
